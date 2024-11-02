5021970c367ecb4e3a39005990bf1f66eef1c038
Thomas Viking Christiansson <160872481+ThomasCsson@users.noreply.github.com>
2024-11-02 16:38:46 +0100

    Update

--- Taylor.py
+++ Taylor.py
@@ -16,21 +16,3 @@ for x in x_values:
 plt.plot(x_values,y_values)
 plt.show()
 
-x = symbols('x')
-
-# Define the function
-f = x**2 + 3*x + 5
-
-# Compute the derivative
-f_prime = diff(f, x)
-
-# Convert the symbolic expressions to functions for numerical evaluation
-f_func = lambdify(x, f, 'numpy')
-f_prime_func = lambdify(x, f_prime, 'numpy')
-
-# Generate x values
-x_vals = np.linspace(-10, 10, 400)
-
-# Calculate y values for the function and its derivative
-y_vals = f_func(x_vals)
-y_prime_vals = f_prime_func(x_vals)
--- tests.py
+++ tests.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 from sympy import sin, symbols, diff, lambdify
 import math
 
-N = 5
+N = 100
 a = 1
 
 x_vals = np.linspace(-10, 10, 400)
@@ -11,21 +11,25 @@ x = symbols('x')
 f = sin(x)
 f_func = lambdify(x, f, 'numpy')
 y_vals = f_func(x_vals)
+final_y = [0]*len(x_vals)
 
 
 for n in range(N):
     f_Nth_der = diff(f, x, n)
     f_prime_func = lambdify(x, f_Nth_der, 'numpy')
     f_Nth = f_prime_func(a)/(math.factorial(n))*(x-a)**n
+
     Nth_function = lambdify(x, f_Nth, 'numpy')
-
     y_prime_vals = Nth_function(x_vals)
-
-    # Plot the function and its derivative
-
-
-    plt.plot(x_vals, y_vals)
-    plt.plot(x_vals, y_prime_vals)
+    if isinstance(y_prime_vals, np.ndarray):
+        plt.plot(x_vals, y_vals)
+        plt.plot(x_vals, y_prime_vals)
+    for i in range(len(final_y)):
+        final_y[i] += Nth_function(x_vals[i])
+plt.show()
+plt.plot(x_vals,final_y)
+plt.show()
+plt.plot(x_vals,y_vals)
 plt.show()
 
-print(f_func(0))
+
